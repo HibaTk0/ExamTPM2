4589dc94c444730eaf7913cd4d21bb808b072947
hiba <hibatoukebri41@gmail.com>
2026-03-10 10:30:38 +0100

    R::evert "Modification de model.py"
    This reverts commit 40bf2a540feab3ecde15636ca64ff75a64062e99.

--- ExamTPM2/model.py
+++ ExamTPM2/model.py
@@ -1,4 +1,1 @@
-Data=[1,3,5]
-print('le produit est :', Prod(Data))
-print('le min est :', min(Data))
-print('le max est :', max(Data))
+Data=[1,3,5]\nProd=math.prod(Data)\nprint('le produit est :', Prod)
